b0943cd89989d9cdea6bc629f6f997493b1f4567
shujian.chen <shujian.chen@moloco.com>
2025-08-14 14:10:03 -0400

    Major packages update

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,6 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).+(ts|js)'],
-  moduleNameMapper: {
-    '^axios$': '<rootDir>/node_modules/axios/dist/node/axios.cjs',
-  },
 };
 
 export default config;
